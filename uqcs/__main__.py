5fe419e0f660dfb52195d21c61c5f83e98023f57
nicklambourne <ndl93@live.com>
2020-02-03 21:37:26 +1100

    new project structure

--- uqcs/__main__.py
+++
@@ -1,5 +0,0 @@
-import sys
-from uqcs import main
-
-if __name__ == "__main__":
-    main(sys.argv)
--- uqcs/templates/__init__.py
+++
@@ -1,6 +0,0 @@
-from mako.lookup import TemplateLookup
-import os
-
-emails_dir = os.path.join(os.path.dirname(__file__), '..', 'emails')
-
-lookup = TemplateLookup([os.path.dirname(__file__), emails_dir])
