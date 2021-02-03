690f8fd5f6c0dde0131edccccc733b27fda75eda
Kenton Lam <kenton_lam@outlook.com>
2021-02-03 21:15:28 +1000

    Add scraping script with instructions.

---
+++ util/README.md
@@ -0,0 +1,13 @@
+This folder contains utilities to assist with the signup form.
+
+Currently, this is just files to scrape the list of programs and majors for
+autocomplete.
+
+## Scrape programs and majors
+
+1. Go to the undergraduate programs page: https://my.uq.edu.au/programs-courses/browse.html?level=ugpg.
+2. Execute the scrape.js script in the browser console and paste the result into ugpg.json.
+3. Repeat 1 and 2 with the postgraduate programs and pasting into pgpg.json from https://my.uq.edu.au/programs-courses/browse.html?level=pgpg.
+4. Manually enter the research programs from https://future-students.uq.edu.au/study/programs?level[Research]=Research&year=2021 into format.py.
+5. Execute format.py which will generate programs.json and programs_with_majors.json.
+6. Copy programs_with_majors.json into /uqcs/static and commit the changes.
---
+++ util/format.py
@@ -0,0 +1,45 @@
+import json
+
+def get_program_name(program):
+    name = program['name']
+    name = name.replace(' - St Lucia', '')
+    name = name.replace(' - Gatton', '')
+    type = program['type']
+    type = type.replace(' (Dual Degree)', 's')
+    type = type.replace(' Honours', '')
+    type = type.replace(' (Extended)', '')
+
+    if 'units)' in name:
+        name = name.split(' (')[0]
+    return f'{type} of {name}'
+
+def format_data(data):
+    out = {}
+    for prog in sorted(data, key=lambda x: x['name']):
+        name = get_program_name(prog)
+        majors = prog['majors']
+        out[name] = majors
+    return out
+
+with open('ugpg.json') as f:
+    ug = json.load(f)
+with open('pgpg.json') as f:
+    pg = json.load(f)
+
+ug2 = format_data(ug)
+pg2 = format_data(pg)
+
+# research programs from https://future-students.uq.edu.au/study/programs?level[Research]=Research&year=2021
+for x in ('Doctor of Biotechnology', 'Master of Philosophy', 'Doctor of Philosophy', 'Doctor of Veterinary Clinical Science'):
+    pg2[x] = None # no specific majors listed.
+
+ug = [(get_program_name(x)) for x in ug]
+ug = list(sorted(set(ug)))
+pg = [(get_program_name(x)) for x in pg]
+pg = list(sorted(set(pg)))
+
+with open('programs.json', 'w') as f:
+    json.dump({'undergrad': ug, 'postgrad': pg}, f, indent=2)
+
+with open('programs_with_majors.json', 'w') as f:
+    json.dump({'undergrad': ug2, 'postgrad': pg2}, f, indent=2)
